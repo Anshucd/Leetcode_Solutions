07ed8893122bedfec5d6177107e5455aa103de63
Anshucd <155870406+Anshucd@users.noreply.github.com>
2026-06-27 04:24:13 +0530

    Time: 0 ms (100%), Space: 31.1 MB (5.39%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -11,21 +11,39 @@ public:
         //  reverse(nums.begin(),nums.end());
         //  reverse(nums.begin(),nums.begin()+k);
         //  reverse(nums.begin()+k,nums.end());
-        vector<int>arr;
-        int n=nums.size();
+        // vector<int>arr;
+        // int n=nums.size();
 
+        // k=k%n;
+        // for(int i=n-k;i<=n-1;i++){
+        //     int value=nums[i];
+        //     arr.push_back(value);
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     if(i-k>=0){
+        //         nums[i]=nums[i-k];
+        //     }
+        // }
+        // for(int i=0;i<k;i++){
+        //     nums[i]=arr[i];
+        // }
+        int n=nums.size();
+        vector<int>ans;
         k=k%n;
-        for(int i=n-k;i<=n-1;i++){
-            int value=nums[i];
-            arr.push_back(value);
+        for(int i=n-k;i<n;i++){
+            ans.push_back(nums[i]);
+            
         }
-        for(int i=n-1;i>=0;i--){
-            if(i-k>=0){
-                nums[i]=nums[i-k];
-            }
+        for(int i=0;i<n-k;i++){
+            ans.push_back(nums[i]);
+
         }
-        for(int i=0;i<k;i++){
-            nums[i]=arr[i];
+        for(int i=0;i<n;i++){
+            nums.pop_back();
+        }
+        
+        for(int i=0;i<n;i++){
+            nums.push_back(ans[i]);
         }
         
     }
